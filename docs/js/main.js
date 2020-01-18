5eb81d9c47339a7ef34484b2ddd099e50fba389c
Elena <info@webstar-support.com>
2020-01-19 00:05:54 +0700

    Six commit

---
+++ docs/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+//mobile navigation
+
+var navToggleButton = $('.navigation__toggle');
+	var navToggleIcon = $('.navigation__toggle .fa');
+	var navBlock = $('.navigation__list');
+	var navBlockOpen = 'navigation__list--open';
+	var navLink = $('.navigation__list a');
+	var navWhite = $('.navigation__toggle--white');
+	var iconNav = 'fa-bars';
+	var iconClose = 'fa-times';
+
+	// Мобильная навигация
+	navToggleButton.on('click', function(e){
+		e.preventDefault();
+		navBlock.toggleClass(navBlockOpen);
+
+		if ( navToggleIcon.hasClass(iconNav) ){
+			navToggleIcon.removeClass(iconNav);
+			navToggleIcon.addClass(iconClose);
+		} else {
+			navToggleIcon.removeClass(iconClose);
+			navToggleIcon.addClass(iconNav);
+		}
+		
+	})
+
+	navLink.on('click', function(){
+
+		if ( navBlock.hasClass(navBlockOpen) ) {
+			if ( navToggleIcon.hasClass(iconNav) ){
+				navToggleIcon.removeClass(iconNav);
+				navToggleIcon.addClass(iconClose);
+				navToggleIcon.addClass(navWhite);
+			} else {
+				navToggleIcon.removeClass(iconClose);
+				navToggleIcon.addClass(iconNav);
+			}
+		}
+
+		navBlock.removeClass(navBlockOpen);
+		
+	
+})
+
+});
